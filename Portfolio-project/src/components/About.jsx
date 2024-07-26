2170d867f7a5082486149936e1e8034db43e7f5c
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2024-07-26 23:45:07 +0500

    width change

--- Portfolio-project/src/components/About.jsx
+++ Portfolio-project/src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <div className="flex flex-wrap">
                 <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <img className='rounded-2xl w-[400px] h-[500px] object-cover ' src={aboutImg} alt="" />
+                        <img className='rounded-2xl w-[400px] lg:h-[500px] h-[400px] object-cover ' src={aboutImg} alt="" />
                     </div>
 
                   
